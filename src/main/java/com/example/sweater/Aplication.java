dff792ac979a650bac7610e61e70cfa8a5381f8e
Liubomyr680 <lshynkaruk8@gmail.com>
2020-01-29 15:20:52 +0200

    initial push

---
+++ src/main/java/com/example/sweater/Aplication.java
@@ -0,0 +1,17 @@
+package com.example.sweater;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+public class Aplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aplication.class, args);
+
+
+    }
+
+}
---
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -0,0 +1,40 @@
+package com.example.sweater.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;                             //Ідентифікатор, для того щоб розрізняти дві записі в одній таблиці
+
+    private String text;
+    private String tag;
+
+    public Message() {}
+
+    public Message(String text, String tag) {
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
